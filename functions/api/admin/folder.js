fc82789cf472328c73556f3fd1498a893243703e
Johna <j-verstraaten@hotmail.com>
2024-02-08 15:52:52 +0000

    Test authentication

--- functions/api/admin/folder.js
+++ functions/api/admin/folder.js
@@ -1,19 +1,21 @@
-export async function onRequestPost(context, request) {
+export async function onRequestPost(context) {
+  const data = await context.request.json();
+
   if (
-    request.body.authToken === context.env.ADMIN_AUTH_TOKEN &&
-    request.body.addFolderAuthToken === context.env.ADMIN_ADD_FOLDER_AUTH_TOKEN
+    data.authToken === context.env.ADMIN_AUTH_TOKEN &&
+    data.addFolderAuthToken === context.env.ADMIN_ADD_FOLDER_AUTH_TOKEN
   ) {
     try {
       context.env.JVPHOTOGRAPHY_DB.prepare(`INSERT INTO folders VALUES (
         ${crypto.randomUUID()},
-        ${request.body.slug},
-        ${request.body.name}
+        ${data.slug},
+        ${data.name}
       )`);
       return new Response("Success", { status: 200 });
     } catch (error) {
       return new Response(error, { status: 500 });
     }
   } else {
-    return Response("401 Unauthorized", { status: 401 });
+    return new Response("401 Unauthorized", { status: 401 });
   }
 }
