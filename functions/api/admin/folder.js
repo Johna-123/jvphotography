99463c103d8f8781e9ece6322f0a50e5a024ae4a
Johna <j-verstraaten@hotmail.com>
2024-02-09 16:09:04 +0000

    Test adding folder

--- functions/api/admin/folder.js
+++ functions/api/admin/folder.js
@@ -1,11 +1,7 @@
 export async function onRequestGet(context) {
   try {
-    context.env.JVPHOTOGRAPHY_DB.prepare(`INSERT INTO folders VALUES (
-      ${crypto.randomUUID()},
-      'test',
-      'Test'
-    )`).run();
-    return new Response("Success", { status: 200 });
+    const message = await context.env.JVPHOTOGRAPHY_DB.prepare("INSERT INTO folders (id, slug, name) VALUES ('16256673-c941-4fd3-b125-8396074be384', 'test', 'Test')").run();
+    return new Response(`Success: ${message}`, { status: 200 });
   } catch (error) {
     return new Response(error, { status: 500 });
   }
