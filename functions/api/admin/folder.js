270890dbd9d9168045220e6b71d9998318159327
Johna <j-verstraaten@hotmail.com>
2024-02-09 16:16:21 +0000

    Test adding folder

--- functions/api/admin/folder.js
+++ functions/api/admin/folder.js
@@ -1,8 +1,21 @@
-export async function onRequestGet(context) {
-  try {
-    const message = await context.env.JVPHOTOGRAPHY_DB.prepare("INSERT INTO folders (id, slug, name) VALUES ('16256673-c941-4fd3-b125-8396074be384', 'test', 'Test')").run();
-    return new Response(`Success: ${message}`, { status: 200 });
-  } catch (error) {
-    return new Response(error, { status: 500 });
+export async function onRequestPost(context) {
+  const data = await context.request.json();
+
+  if (
+    data.authToken === context.env.ADMIN_AUTH_TOKEN &&
+    data.addFolderAuthToken === context.env.ADMIN_ADD_FOLDER_AUTH_TOKEN
+  ) {
+    try {
+      const response = await context.env.JVPHOTOGRAPHY_DB.prepare(`INSERT INTO folders VALUES (
+        ${crypto.randomUUID()},
+        ${data.slug},
+        ${data.name}
+      )`);
+      return new Response(response, { status: 200 });
+    } catch (error) {
+      return new Response(error, { status: 500 });
+    }
+  } else {
+    return new Response("401 Unauthorized", { status: 401 });
   }
 }
