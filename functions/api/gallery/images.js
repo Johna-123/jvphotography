6a8a9c2be237a4dfe6d139f59cec2a3feb0139e4
Johna <j-verstraaten@hotmail.com>
2024-02-09 22:34:39 +0000

    Remove unnecessary columns in images API

--- functions/api/gallery/images.js
+++ functions/api/gallery/images.js
@@ -1,5 +1,5 @@
 export async function onRequestGet(context) {
-  const images = context.env.JVPHOTOGRAPHY_DB.prepare("SELECT * FROM images");
+  const images = context.env.JVPHOTOGRAPHY_DB.prepare("SELECT id, slug, name, tags, folder FROM images");
   const data = await images.all();
 
   return Response.json(data);
