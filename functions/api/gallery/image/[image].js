4dc6910bc3d892fc28ca885da067ef76c62b4c20
Johna <j-verstraaten@hotmail.com>
2024-02-10 10:07:13 +0000

    Test photo page

--- functions/api/gallery/image/[image].js
+++ functions/api/gallery/image/[image].js
@@ -1,5 +1,5 @@
 export async function onRequestGet(context) {
-  const image = context.env.JVPHOTOGRAPHY_DB.prepare("SELECT * FROM images WHERE id = ?").bind(context.params.image);
+  const image = context.env.JVPHOTOGRAPHY_DB.prepare("SELECT * FROM images WHERE id = ?").bind(context.params.id);
   const data = await image.first();
 
   return Response.json(data);
