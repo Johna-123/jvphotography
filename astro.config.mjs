3c3909752be456dbc67e8388333804d80b88c126
Johna <j-verstraaten@hotmail.com>
2024-02-20 10:24:41 +0000

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://jvphotography.pages.dev",
+  integrations: [
+    sitemap({
+      filter: (page) =>
+        page !== "https://jvphotography.pages.dev/admin/add_image" &&
+        page !== "https://jvphotography.pages.dev/admin/add_image/" &&
+        page !== "https://jvphotography.pages.dev/admin/add_folder" &&
+        page !== "https://jvphotography.pages.dev/admin/add_folder/",
+    }),
+  ],
+});
