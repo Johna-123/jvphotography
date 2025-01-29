23b70b36c0154a1889aad846558424964a3487e0
Johna <j-verstraaten@hotmail.com>
2025-01-29 09:53:04 +0000

    Clear repository

--- functions/api/gallery/folders.js
+++
@@ -1,6 +0,0 @@
-export async function onRequestGet(context) {
-  const folders = context.env.JVPHOTOGRAPHY_DB.prepare("SELECT * FROM folders");
-  const data = await folders.all();
-
-  return Response.json(data);
-}
--- functions/api/gallery/images.js
+++
@@ -1,6 +0,0 @@
-export async function onRequestGet(context) {
-  const images = context.env.JVPHOTOGRAPHY_DB.prepare("SELECT id, slug, name, tags, folder FROM images");
-  const data = await images.all();
-
-  return Response.json(data);
-}
